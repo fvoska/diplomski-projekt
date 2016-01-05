1702c6248a6a4fbb235f85b12d2a83ad43681189
Filip Voska <filip.voska@gmail.com>
2016-01-05 20:03:34 +0100

    fix port for heroku

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var app = express();
 app.use(express.static('client'));
 app.use(function(req, res) {
   // Use res.sendfile, as it streams instead of reading the file into memory.
-  res.sendfile(__dirname + '/client/index.html');
+  res.sendFile(__dirname + '/client/index.html');
 });
 
-var server = app.listen(80, function () {
+var server = app.listen(process.env.PORT || 80, function () {
   var host = server.address().address;
   var port = server.address().port;
 
