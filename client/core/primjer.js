2a2b1dd09fac9c746156472302bfbfef78fd1dc5
Filip Voska <filip.voska@gmail.com>
2016-01-17 11:47:19 +0100

    clean primjer.js

--- client/core/primjer.js
+++ client/core/primjer.js
@@ -1,21 +1,4 @@
 {
-  "requestTime": "2016-01-13 17:31:32.771673",
-  "wordCounter": 1,
-  "ip": "161.53.78.35",
-  "userID": "51D4F266-B4B7-11E5-8CB1-C97843B759E8",
-  "context": "on",
-  "report": {
-    "errors": [{
-      "suspicious": "\u010dovijek",
-      "suggestions": ["\u010dovjek", "dovijek"],
-      "severity": "mm",
-      "occurrences": "1"
-    }],
-    "numberOfErrors": 1
-  }
-}
-
-{
   "timeProcessed": 4.2,
   "requestTime": "2015-01-01 08:36:25.000000",
   "wordCounter": 28,
