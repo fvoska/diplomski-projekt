1de862e02992efa49c1ee33e87c0fd7a38c5ddea
Filip Voska <filip.voska@gmail.com>
2016-01-17 10:47:34 +0100

    .

--- client/includes/js/config.js
+++ client/includes/js/config.js
@@ -1,6 +1,6 @@
 var config = {
-  //baseUrl: '/grupa84/app',
-  baseUrl: '/diplomski/client',
+  baseUrl: '/grupa84/app',
+  //baseUrl: '/diplomski/client',
   language: 'hr',
   slideDownSpeed: 250,
   slideUpSpeed: 250
